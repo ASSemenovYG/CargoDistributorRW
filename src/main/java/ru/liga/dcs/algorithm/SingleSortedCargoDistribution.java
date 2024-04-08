5627e1684c1c086cef9c9ad05d0af262c0275972
ASSemenovYG <assemenov@fil-it.ru>
2024-04-08 17:35:29 +0300

    SingleSortedCargoDistribution added logging

--- src/main/java/ru/liga/dcs/algorithm/SingleSortedCargoDistribution.java
+++ src/main/java/ru/liga/dcs/algorithm/SingleSortedCargoDistribution.java
@@ -1,5 +1,7 @@
 package ru.liga.dcs.algorithm;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import ru.liga.dcs.cargo.CargoItem;
 import ru.liga.dcs.cargo.CargoList;
 import ru.liga.dcs.cargo.CargoVan;
@@ -16,6 +18,8 @@ import java.util.List;
  * <br>Посылки ставятся друг на друга, пока есть место в грузовом фургоне, если нет - переход к следующему фургону
  */
 public class SingleSortedCargoDistribution extends DistributionAlgorithm {
+    private static final Logger LOGGER = LogManager.getLogger(SingleSortedCargoDistribution.class);
+
     public SingleSortedCargoDistribution(CargoList cargoList) {
         super(cargoList);
     }
@@ -27,6 +31,7 @@ public class SingleSortedCargoDistribution extends DistributionAlgorithm {
         cargoItems.sort(Comparator.comparing(CargoItem::getWidth).reversed().thenComparing(CargoItem::getLength));
 
         while (!cargoItems.isEmpty()) {
+            LOGGER.debug("Создаю новый грузовой фургон");
             CargoVan van = new CargoVan();
             boolean vanIsNotFull = true;
             int currentLengthCoordinate;
@@ -34,18 +39,25 @@ public class SingleSortedCargoDistribution extends DistributionAlgorithm {
             while (vanIsNotFull) {
                 CargoItem cargoItem = cargoItems.peekFirst();
                 if (cargoItem != null) {
+                    LOGGER.debug("Обработка посылки:\n" + cargoItem.getName());
                     currentLengthCoordinate = nextLengthCoordinate;
+                    LOGGER.debug("Попытка установить посылку на строку " + currentLengthCoordinate);
                     vanIsNotFull = van.tryPuttingCargoItemAtCoordinates(cargoItem, currentLengthCoordinate, 0);
+                    LOGGER.debug(vanIsNotFull ? "Посылка успешно добавлена в фургон" : "Не удалось загрузить посылку в грузовой фургон");
                     if (vanIsNotFull) {
                         nextLengthCoordinate = currentLengthCoordinate + cargoItem.getLength();
+                        LOGGER.debug("Номер строки для добавления следующей посылки " + nextLengthCoordinate);
                         cargoItems.removeFirst();
                     }
                 } else {
+                    LOGGER.debug("Перехожу к следующему фургону");
                     break;
                 }
             }
+            LOGGER.debug("Добавляю фургон в список загруженных");
             result.add(van);
         }
+        LOGGER.info("Количество загруженных фургонов: " + result.size());
         return result;
     }
 }
