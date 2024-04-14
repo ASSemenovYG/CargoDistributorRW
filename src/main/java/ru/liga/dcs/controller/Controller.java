ebac76a201e1d53b4e362a636723559d6956d9f5
ASSemenovYG <assemenov@fil-it.ru>
2024-04-15 00:23:08 +0300

    added CargoDistributorApplication

---
+++ src/main/java/ru/liga/dcs/controller/Controller.java
@@ -0,0 +1,12 @@
+package ru.liga.dcs.controller;
+
+import org.springframework.shell.standard.ShellComponent;
+import org.springframework.shell.standard.ShellMethod;
+
+@ShellComponent
+public class Controller {
+    @ShellMethod("hello")
+    public void hello() {
+        System.out.println("Hello");
+    }
+}
